ec44d00db81eb7b670eee0d80ce2741ce3202216
LeeeYumin <flet39@daum.net>
2024-01-16 17:25:43 +0900

    1.16 회원가입 후 완료메세지 뜨게. 로그인 후 로그아웃 보이게 해결

--- BookJeok/src/main/java/com/yedam/member/command/JoinControl.java
+++ BookJeok/src/main/java/com/yedam/member/command/JoinControl.java
@@ -8,18 +8,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedam.common.Control;
+import com.yedam.member.service.MemberService;
+import com.yedam.member.serviceImpl.MemberServiceImpl;
+import com.yedam.member.vo.MemberVO;
 
 public class JoinControl implements Control {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/member/join.jsp");
-		try {
-			rd.forward(req, resp); // 요청을 재 지정하겠습니다.
-		} catch (ServletException | IOException e) {
-			e.printStackTrace();
-		}
-	}
+		// parameter(3개), DB insert, 목록페이지.
+		MemberVO vo = new MemberVO(); // 기본생성자 호출
+		if (req.getMethod().equals("GET")) {
+			// GET 방식 요청일 때
+			// 3개 : title=테스트&content=입력내용&writer=홍길동
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/member/memberjoin.jsp");
+			try {
+				rd.forward(req, resp); // 요청을 재 지정하겠습니다.
+			} catch (ServletException | IOException e) {
+				e.printStackTrace();
+			}
+
+		} else if (req.getMethod().equals("POST")) {
+
+			String id = req.getParameter("id");
+			String mName = req.getParameter("mName");
+			String pw = req.getParameter("pw");
+			String email = req.getParameter("email");
+			String age = req.getParameter("age");
+			String phone = req.getParameter("phone");
+			String likes = req.getParameter("likes");
+			String sms = req.getParameter("sms");
+
+			vo = new MemberVO(); // 기본생성자 호출
+			vo.setId(id);
+			vo.setMName(mName);
+			vo.setPw(pw);
+			vo.setEmail(email);
+			vo.setAge(age);
+			vo.setPhone(phone);
+			vo.setLikes(likes);
+			vo.setSms(sms);
+
+			MemberService svc = new MemberServiceImpl();
+			try {
+				if (svc.joinMember(vo)) {
+					resp.sendRedirect("main.do"); //Joinresult.do
+					req.setAttribute("message", "환영합니다");
+				} else {
+					resp.sendRedirect("memberJoin.do");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} // end of if
+
+	} // end of execute()
 
 }
--- BookJeok/src/main/java/com/yedam/member/mapper/MemberMapper.java
+++ BookJeok/src/main/java/com/yedam/member/mapper/MemberMapper.java
@@ -1,8 +1,11 @@
 package com.yedam.member.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.yedam.member.vo.MemberVO;
 
 public interface MemberMapper {
 
 	public int insertMember(MemberVO vo);
+	public MemberVO selectMember(@Param("id") String id, @Param("pw") String pw);
 }
